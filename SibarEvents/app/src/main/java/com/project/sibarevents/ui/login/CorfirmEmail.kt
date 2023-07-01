74f1b98b2cd2a50698b832c4420f141693283c7e
Alexis00002521 <91161370+Alexis00002521@users.noreply.github.com>
2023-07-01 15:01:25 -0600

    vista de creation

--- SibarEvents/app/src/main/java/com/project/sibarevents/ui/login/CorfirmEmail.kt
+++ SibarEvents/app/src/main/java/com/project/sibarevents/ui/login/CorfirmEmail.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.navigation.findNavController
 import com.project.sibarevents.R
 
 
 class CorfirmEmail : Fragment() {
 
-
+    private lateinit var backButton: ImageButton
+    private lateinit var nextButton: ImageButton
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,6 +21,20 @@ class CorfirmEmail : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_corfirm_email, container, false)
     }
+    private fun bind() {
+        nextButton = view?.findViewById(R.id.btnNext) as ImageButton
+        backButton = view?.findViewById(R.id.btnBack) as ImageButton
 
+    }
+    private fun listeners(){
+
+        nextButton.setOnClickListener {
+            it.findNavController().navigate(R.id.action_forgotPassword_to_corfirmEmail)
+        }
+        backButton.setOnClickListener {
+            it.findNavController().navigate(R.id.action_forgotPassword_to_login)
+        }
+
+    }
 
 }
